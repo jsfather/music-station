41fd2740ee6e772c5be9541dea24f9a2f8465ce3
jsfather <keyvanmatinrad@outlook.com>
2024-07-31 16:17:34 +0330

    tailwind.config.js added

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#10F9B1'
+            }
+        }
+    }
+}
+
