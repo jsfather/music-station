db84f869c91102e296c1116d2b449b5de34b7c3a
jsfather <keyvanmatinrad@outlook.com>
2024-07-31 15:30:35 +0330

    ui added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,13 @@
 export default defineNuxtConfig({
-    modules: ["@nuxtjs/tailwindcss", "@formkit/auto-animate/nuxt", "@nuxt/image"]
+    modules: [
+        "@nuxtjs/tailwindcss",
+        "@formkit/auto-animate/nuxt",
+        "@nuxt/image",
+        "@nuxt/icon",
+        "@vueuse/nuxt",
+        "@nuxt/scripts",
+        "nuxt-headlessui",
+        "@vee-validate/nuxt",
+        "@nuxt/ui"
+    ]
 })
